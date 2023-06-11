2a087644e0d1178df76f544ded96406d8b0c79a4
Hector <torrezhectorb@gmail.com>
2023-06-10 20:54:30 -0600

    finish

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line react/prop-types
 export const Alert = ({ message }) => {
-  console.log(message);
   return (
     <section className="border border-red-800 w-screen">
       <h3 className="text-lg text-red-800 font-semibold">{message}</h3>
